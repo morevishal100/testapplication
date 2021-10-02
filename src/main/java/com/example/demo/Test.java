88e3b0743256208a6a9f9d14c9753ad2ad3852b2
vishal_dev <vmore100@gmail.com>
2021-10-02 19:38:53 +0530

    Modifying Test class

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+@RestController
+public class Test  {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
-
+   @GetMapping("/test")
+   public String  helloWorld() {
+       return "Hello World";
+   }	
+ 
 }
